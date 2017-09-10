4892b8a980eb0e12a33d00aeac65525c0d79371a
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-09-10 20:42:57 +0430

    Remove Useless Logs

--- common/models/starter.js
+++ common/models/starter.js
@@ -5,13 +5,7 @@ var app = require('../../server/server')
 module.exports = function(starter) {
 
 	var startServices = cron.job("0 */1 * * * *", function () {
-		shell.exec('/root/./startScript.sh', function(code, stdout, stderr) {
-			console.log('EXIT CODE: ', code)
-			console.log('PROGRAM OUTPUT:')
-			console.log(stdout)
-			console.log('PROGRAM STDERR:')
-			console.log(stderr)
-		})
+		shell.exec('/root/./startScript.sh')
 	})
 
 	startServices.start()
